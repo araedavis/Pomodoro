9a8c0e18b2a3c227128779d7d36b81e15311fea4
Allison Davis <a.rae.davis@gmail.com>
2015-12-16 20:06:40 -0800

    added ui functions to reset and break buttons; cleaned up timer functions; combined all js into app.js file

--- js/app.js
+++ js/app.js
@@ -1,14 +1,67 @@
-$(document).ready(function() {
-  
+$(document).ready(function(){
 
-  /* $('#timer').html(timerHTML); //this renders the minutes and seconds as properly formatted HTML */
+var minutes = 25;
+var seconds = 0;
+
+function displayTime(){
+    var timerHTML = "<h2>" + minutes + ":" + seconds + "</h2>";
     
+    if (seconds < 10) { //adds leading zero for seconds
+       timerHTML = "<h2>" + minutes + ":0" + seconds + "</h2>"; ;
+    }
 
- 
+    $('#timer').html(timerHTML);
+  }
+
+function countdown(){
+    seconds--;
+    if (seconds < 0) {
+      seconds = 59;
+      minutes--;
+    }
+    displayTime();
+  }
+
+  displayTime();
+	
+
+	//plus & minus button controls
+      //need to add logic to prevent negative values
+      //logic for plus and minus buttons to add to minutes when counter is paused?
+
+	$(".plus").click(function() {
+      minutes++;
+      displayTime();
+    });
+
+    $(".minus").click(function() {
+      minutes--;
+      displayTime();
+    });
 
 
+   //start, pause, refresh, break controls 
+   $(".start").click(function(){
+   		$('.start').toggleClass('paused');
+     	$(".start-i").toggleClass('fa-play fa-pause');
+
+
+      var interval = setInterval(countdown, 1000);
+
+      //still have to figure out how to clear interval / pause timer 
     
- 
+      
+   });
+
+   $(".refresh").click(function(){
+      minutes = "00";
+      displayTime();
+   });
+
+   $(".break").click(function(){
+      minutes = 5;
+      displayTime();
+   });
+      
+
 });
-
-//so the pause button works, although the paused time needs to be formatted. BUT how to prevent the interval from then resetting? Tried adding toggle classes - didn't help. 
